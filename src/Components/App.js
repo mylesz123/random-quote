ba8519f39db54c000671da872bc463fe138bdbd2
Myles Young <myles.young@mailchimp.com>
2020-02-15 00:54:46 -0500

    add random colors the shuffle button

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,11 +3,14 @@ import '../css/App.css';
 
 import Quote from '../Components/Quote';
 import {quotes} from '../data/quotes';
+import {colors} from '../data/colors';
 
 export default function App() {
 
   let [shuffle, setShuffle] = useState(0);
 
+  const getRandomColor = Math.floor(Math.random() * colors.length);
+  
   const shuffleQuotes = () => {
     let getRandomIndex = Math.floor(Math.random() * quotes.length);
     setShuffle(shuffle = getRandomIndex);
@@ -19,7 +22,14 @@ export default function App() {
         key={quotes[shuffle]}
         {...quotes[shuffle]}
       />
-      <button id="loadQuote" onClick={shuffleQuotes}>Show another quote</button>
+      <button id="loadQuote" 
+        onClick={shuffleQuotes}
+        style={{
+          background: colors[getRandomColor]
+        }}
+      >
+        Show another quote
+      </button>
     </div>
   );
 }
